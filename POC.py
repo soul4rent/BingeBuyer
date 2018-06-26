7d921fb3da0ccbec5d7e5828bbac90619236cd84
soul4rent <kyle.a.wadsworth@gmail.com>
2018-06-25 18:51:17 -0700

    Got it to generate list of nouns mentioned in youtube videos

--- POC.py
+++ POC.py
@@ -1,6 +1,7 @@
 from amazon.api import AmazonAPI
 from nltk.corpus import wordnet
 from pytube import YouTube
+import re
 
 #FIRST TIME USAGE: MUST DOWNLOAD DICTIONARY
 #import nltk
@@ -9,13 +10,27 @@ from pytube import YouTube
 ytlink = input("Youtube Link:")
 yt = YouTube(ytlink)
 caption = yt.captions.get_by_language_code('en')
-print(caption.generate_srt_captions()) #youtube captions
+justTextCaption = re.sub("[<].*?[>]", " ", caption.generate_srt_captions())
+#Because regex is easy to write and impossible to read sometimes:
+#- Match a single character "<" ( [<] )
+#- Match any character ( . ), 0-infinite times ( * ), lazily, AKA NOT GETTING EVERYTHING ( ? )
+#- Match a single character ">" ( [>] )
 
 
+#noTag Captions
+print(justTextCaption) #youtube captions
 
+
+print("-Generating list of nouns from dictionary")
 
 nouns = {x.name().split('.', 1)[0] for x in wordnet.all_synsets('n')}
 #create set of nouns
 
 #TODO: LIST NOUNS IN CAPTIONS
-print("test" in nouns)
+print("-Generating caption list")
+captionList = re.split('\n|[ ]',justTextCaption)
+print(captionList)
+
+for word in captionList:
+    if (word in nouns):
+        print(word + " ::: SUCCESS!")
